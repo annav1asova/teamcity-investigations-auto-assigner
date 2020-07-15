8e7959cb7341ee178c070da65f21032b72637563
Anna Vlasova <annasv209@gmail.com>
2020-07-15 12:34:08 +0300

    Update DefaultUserResponsibilityTest.java

--- src/test/java/jetbrains/buildServer/investigationsAutoAssigner/common/DefaultUserResponsibilityTest.java
+++ src/test/java/jetbrains/buildServer/investigationsAutoAssigner/common/DefaultUserResponsibilityTest.java
@@ -29,6 +29,7 @@ public class DefaultUserResponsibilityTest {
     SUser user = Mockito.mock(SUser.class);
     Responsibility responsibility = new DefaultUserResponsibility(user);
 
-    assertTrue(responsibility.getDescription().contains("default"));
+    assertFalse(responsibility.getDescription().contains("default"));
+   
   }
 }
